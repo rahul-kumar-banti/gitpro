6a3e18558060e448c5a278ffa63970af22b9de02
rahul <rahulbanti36@gmail.com>
2019-11-18 14:14:53 +0530

    rought template

---
+++ js/login-signup.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+   $(".loginframe").hide();
+   $(".regframe").hide();
+   $(".lgbtn").click(function(){
+    $(".loginframe").show();  
+   })
+   $(".rgbtn").click(function(){
+    $(".regframe").show();  
+   })
+   $(".closebtn").click(function(){
+    $(".loginframe").hide(); 
+    $(".regframe").hide();  
+   })
+   $(document).dblclick(function(){
+    $(".loginframe").hide();
+    $(".regframe").hide();
+   })
+$(".lgfromreg").click(function(){
+    $(".loginframe").show(); 
+    $(".regframe").hide(); 
+})
+$(".lgtoreg").click(function(){
+    $(".loginframe").hide(); 
+    $(".regframe").show(); 
+})
+
+
+})
